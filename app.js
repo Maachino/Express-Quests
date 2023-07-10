e4d0eca27837ac46b491868584c8be86c586c79c
Maachino <maach.med@yahoo.com>
2023-07-10 10:29:33 +0200

    Quete 8 Ok

--- app.js
+++ app.js
@@ -15,42 +15,26 @@ const movieHandlers = require("./movieHandlers");
 const { validateMovie, validateUser } = require("./validators.js");
 const { hashPassword, verifyPassword, verifyToken } = require("./auth");
 
+const userHandlers = require("./userHandlers");
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+
 app.post(
   "/api/login",
   userHandlers.getUserByEmailWithPasswordAndPassToNext,
   verifyPassword
 );
+
 app.use(verifyToken);
+
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
-
-const { hashPassword } = require("./auth.js");
-
-// ...
-
-const userHandlers = require("./userHandlers");
-
-const userHandlers = require("./userHandlers");
-const { hashPassword, verifyPassword } = require("./auth");
-
-// ...
-
-// ...
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post(
-  "/api/login",
-  userHandlers.getUserByEmailWithPasswordAndPassToNext,
-  verifyPassword
-);
-app.use(verifyToken);
-app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
